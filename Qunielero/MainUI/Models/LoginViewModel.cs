6f76d30e8b7014771d4046605be1608b5002fa9e
JULIO-PC\Marquez <marquezisc@gmail.com>
2023-08-24 00:11:46 -0600

    View login.

---
+++ Qunielero/MainUI/Models/LoginViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Quinieleros.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quinieleros.Models
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        private string usuario;
+        private string contrasenia;
+        public string Usuario
+        {
+            get => usuario;
+            set
+            {
+                if (value == usuario) return;
+                usuario = value;
+                //OnPropertyChanged(nameof(Usuario));
+            }
+        }
+        public string Contrasenia
+        {
+            get => contrasenia;
+            set
+            {
+                if (value == contrasenia) return;
+                contrasenia = value;
+                //OnPropertyChanged(nameof(Contrasenia));
+            }
+        }
+    }
+}
--- Qunielero/MainUI/Views/Login.xaml.cs
+++ Qunielero/MainUI/Views/Login.xaml.cs
@@ -1,9 +1,56 @@
+using Quinieleros.Models;
+using System.ComponentModel;
+
 namespace Quinieleros.Views;
 
 public partial class Login : ContentPage
 {
-	public Login()
+    #region Members
+    private string usuario;
+    private string contrasenia;
+    //private LoginViewModel model;
+    #endregion
+
+    #region Properties
+    public string Usuario
+    {
+        get => usuario;
+        set
+        {
+            if (value == usuario) return;
+            usuario = value;
+            OnPropertyChanged(nameof(Usuario));
+        }
+    }
+    public string Contrasenia
+    {
+        get => contrasenia;
+        set
+        {
+            if (value == contrasenia) return;
+            contrasenia = value;
+            OnPropertyChanged(nameof(Contrasenia));
+        }
+    }
+    #endregion
+
+    #region Ctor
+    public Login()
 	{
 		InitializeComponent();
+        //model = new LoginViewModel();
 	}
+    #endregion
+
+    #region Methods
+
+    #endregion
+
+    #region Events
+    private void btnLogin_Clicked(object sender, EventArgs e)
+    {
+        //model.Usuario = "Juls";
+        Usuario = "Dad";
+    }
+    #endregion
 }

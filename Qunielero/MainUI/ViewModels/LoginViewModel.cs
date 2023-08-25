1324c8f25f80a77f9bbe1b2699939186348cef11
JULIO-PC\Marquez <marquezisc@gmail.com>
2023-08-25 01:04:45 -0600

    Integracion de comandos.

---
+++ Qunielero/MainUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,83 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Xml.Linq;
+
+namespace Quinieleros.ViewModels
+{
+    public class LoginViewModel : ObservableObject, IQueryAttributable
+    {
+        #region Members
+        private string usuario;
+        private string contrasenia;
+        #endregion
+
+        #region Properties
+        public string Usuario
+        {
+            get => usuario;
+            set
+            {
+                if (value == usuario) return;
+                usuario = value;
+                OnPropertyChanged(nameof(Usuario));
+                ((Command)LoginCommand).ChangeCanExecute();
+            }
+        }
+        public string Contrasenia
+        {
+            get => contrasenia;
+            set
+            {
+                if (value == contrasenia) return;
+                contrasenia = value;
+                OnPropertyChanged(nameof(Contrasenia));
+                (LoginCommand as Command).ChangeCanExecute();
+            }
+        }
+        #endregion
+
+        #region Ctor
+        public LoginViewModel()
+        {
+            LoginCommand = new RelayCommand(Login, LoginCanExecute);
+
+            ResetTemplate();
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand { get; private set; }
+        #endregion
+
+        #region CanExecute
+        private bool LoginCanExecute() => usuario.Length > 3 && !string.IsNullOrEmpty(contrasenia);
+        #endregion
+
+        #region Methods
+        private void ResetTemplate() 
+        {
+            usuario = string.Empty;
+            contrasenia = string.Empty;
+        }
+        private void Login() 
+        {
+        
+        }
+        #endregion
+        public void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query.ContainsKey("load"))
+            {
+                OnPropertyChanged(nameof(Usuario));
+                OnPropertyChanged(nameof(Contrasenia));
+            }
+        }
+    }
+}
--- Qunielero/MainUI/Views/Login.xaml.cs
+++ Qunielero/MainUI/Views/Login.xaml.cs
@@ -1,34 +1,12 @@
-using Quinieleros.Models;
+using Quinieleros.ViewModels;
 using System.ComponentModel;
 
 namespace Quinieleros.Views;
 
 public partial class Login : ContentPage
 {
-    #region Members
-    public LoginViewModel viewModel;
-    #endregion
-
-    #region Properties
-    #endregion
-
-    #region Ctor
-    public Login()
+	public Login()
 	{
 		InitializeComponent();
-        viewModel = new LoginViewModel();
-        BindingContext = viewModel;
 	}
-    #endregion
-
-    #region Methods
-
-    #endregion
-
-    #region Events
-    private void btnLogin_Clicked(object sender, EventArgs e)
-    {
-        viewModel.Usuario = "Juls";
-    }
-    #endregion
 }

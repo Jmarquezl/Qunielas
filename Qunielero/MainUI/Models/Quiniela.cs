096e38a441e477274fcbe186a3076c491d0aeefb
JULIO-PC\Marquez <marquezisc@gmail.com>
2023-09-22 00:42:48 -0600

    Listado de quinielas.

---
+++ Qunielero/MainUI/Models/Quiniela.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quinieleros.Models
+{
+    public class Quiniela
+    {
+        #region Properties
+        public int Jornada { get; set; }
+        public string Quinielero { get; set; }
+        public int Puntos { get; set; }
+        public string Resumen { get; set; }
+        #endregion
+    }
+}
--- Qunielero/MainUI/ViewModels/RankViewModel.cs
+++ Qunielero/MainUI/ViewModels/RankViewModel.cs
@@ -1,24 +1,44 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Quinieleros.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Quinieleros.ViewModels
 {
-    public class RankViewModel : ObservableObject, IQueryAttributable
+    public partial class RankViewModel : ObservableObject, IQueryAttributable
     {
         #region Members
 
         #endregion
 
         #region Properties
-
+        [ObservableProperty]
+        public ObservableCollection<Quiniela> quinielas;
         #endregion
 
         #region Ctor
-
+        public RankViewModel()
+        {
+            Image image = new Image
+            {
+                Source = ImageSource.FromFile("dotnet_bot.png")
+            };
+            quinielas = new ObservableCollection<Quiniela>();
+            quinielas.Add(new Quiniela() { Jornada = 8, Quinielero = "Julio Márquez", Puntos = 10, Resumen = $"J-{8} Puntos: {10}"});
+            quinielas.Add(new Quiniela() { Jornada = 8, Quinielero = "Julio Márquez", Puntos = 10, Resumen = $"J-{8} Puntos: {10}" });
+            quinielas.Add(new Quiniela() { Jornada = 8, Quinielero = "Julio Márquez", Puntos = 10, Resumen = $"J-{8} Puntos: {10}" });
+            quinielas.Add(new Quiniela() { Jornada = 8, Quinielero = "Julio Márquez", Puntos = 10, Resumen = $"J-{8} Puntos: {10}" });
+            quinielas.Add(new Quiniela() { Jornada = 8, Quinielero = "Julio Márquez", Puntos = 10, Resumen = $"J-{8} Puntos: {10}" });
+            quinielas.Add(new Quiniela() { Jornada = 8, Quinielero = "Julio Márquez", Puntos = 10, Resumen = $"J-{8} Puntos: {10}" });
+            quinielas.Add(new Quiniela() { Jornada = 8, Quinielero = "Julio Márquez", Puntos = 10, Resumen = $"J-{8} Puntos: {10}" });
+            quinielas.Add(new Quiniela() { Jornada = 8, Quinielero = "Julio Márquez", Puntos = 10, Resumen = $"J-{8} Puntos: {10}" });
+            quinielas.Add(new Quiniela() { Jornada = 8, Quinielero = "Julio Márquez", Puntos = 10, Resumen = $"J-{8} Puntos: {10}" });
+            quinielas.Add(new Quiniela() { Jornada = 8, Quinielero = "Julio Márquez", Puntos = 10, Resumen = $"J-{8} Puntos: {10}" });
+        }
         #endregion
 
         #region Commands

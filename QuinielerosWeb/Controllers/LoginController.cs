cc50343ad363683725a8a51099235d66ba3cfa2d
JULIO-PC\Marquez <marquezisc@gmail.com>
2025-02-02 01:54:35 -0600

    Login View.

---
+++ QuinielerosWeb/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuinielerosWeb.Models;
+
+namespace QuinielerosWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login() 
+        {
+            LoginModel model = new LoginModel();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Login(LoginModel model) {
+
+            return View(model);
+        }
+    }
+}
---
+++ QuinielerosWeb/Models/LoginModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuinielerosWeb.Models
+{
+    public class LoginModel
+    {
+        public string Usuario { get; set; } = string.Empty;
+        public string Pass { get; set; } = string.Empty;
+    }
+}

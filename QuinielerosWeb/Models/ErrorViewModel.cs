e3502d1008bda667ec750f7d8746226bf28c3657
JULIO-PC\Marquez <marquezisc@gmail.com>
2025-02-02 00:20:36 -0600

    QuinielerosWeb MVC.

---
+++ QuinielerosWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuinielerosWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
